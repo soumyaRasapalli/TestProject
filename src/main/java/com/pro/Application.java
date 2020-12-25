c9902670babe75b90777af057a7cb623c58c50b8
soumyaRasapalli <76591137+soumyaRasapalli@users.noreply.github.com>
2020-12-25 21:53:39 +0530

    feature2 branch changes

--- src/main/java/com/pro/Application.java
+++ src/main/java/com/pro/Application.java
@@ -22,7 +22,7 @@ public class Application {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		System.out.println("Feature branch changes");
+		System.out.println("Feature2 branch changes");
 
 	}
 
